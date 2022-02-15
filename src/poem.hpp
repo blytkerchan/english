4a74b8f425505da68766640feff8ab4dc75d9506
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-02-14 20:17:17 -0500

    add poet, rename former poet to poem

--- src/poem.hpp
+++ src/poem.hpp
@@ -1,11 +1,12 @@
 #ifndef vlinder_english_poet_hpp
 #define vlinder_english_poet_hpp
 
+#include "english-export.h"
 #include <string>
 #include <vector>
 
 namespace Vlinder { namespace English {
-std::string createPoem(std::vector< unsigned char > const &data);
+std::string createPoem(std::vector< unsigned char > const &data) english_EXPORT;
 }}
 
 #endif
